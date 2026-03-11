060f225cef0255f3148c94463f1352c49a51a55c
RayAfolabi <rachaelsadegenesis@gmail.com>
2026-03-11 05:02:35 -0700

    Add program that prints message using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/*
+*Author: Afolabi Rachael
+*Program: WinMingle Community C Training
+*Description: This is a C program that prints "Programming is like building a multilingual puzzel" followed by a new line.
+*/
+#include <stdio.h>
+int main(void) {
+    puts("\"programming is like building a multilingual puzzel\n");
+    return(0);
+}
+
+
