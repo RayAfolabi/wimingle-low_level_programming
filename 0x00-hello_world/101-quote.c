50f8fe7fca0f0073aacf87349e7c172f13a08343
RayAfolabi <rachaelsadegenesis@gmail.com>
2026-03-11 05:26:49 -0700

    Add a program that print a message without using the printf or puts function and returns 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+/* 
+*Author: Afolabi Rachael
+*Program: WinMingle Community C Training
+*Description: This is a program that prints exactly:and that piece of art is useful" -Dora Korpa,2015-10-19
+*/
+#include <unistd.h>
+int main(void){
+write(2,"and that piece of art is useful\" - Dora Korpa, 2015-10-19\n",59);
+return (1);
+}
