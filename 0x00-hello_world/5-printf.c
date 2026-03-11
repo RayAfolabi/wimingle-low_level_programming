67d1db1f580481f86e1bc37c0a732d422369e8e2
RayAfolabi <rachaelsadegenesis@gmail.com>
2026-03-11 05:05:29 -0700

    Add a program that print a message using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+/*
+*Author: Afolabi Rachael
+*Program: WinMingle Community C Training
+*Description: This is a C program that prints "with proper grammer, but the outcome is a piece of art" it must be followed by a new line */
+#include <stdio.h>
+int main() {
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
