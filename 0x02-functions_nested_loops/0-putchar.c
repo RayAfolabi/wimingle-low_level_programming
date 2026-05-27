14b1a2a2fde0b22030bd7bcd7eebd924544adbd1
RayAfolabi <rachaelsadegenesis@gmail.com>
2026-05-27 11:13:44 -0700

    fix print _putchar to the screen

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
 /*
 *Author: Afolabi Rachael
 * Program: WinMingle Community C Training
 * Description:This is a program that prints _putchar to the screen followed by a new line
 */
-
-#include "main.h"
-#include <stdio.h>
-
 int main(void)
 {
     int i =0;
