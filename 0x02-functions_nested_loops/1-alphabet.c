2282214b18441d8378ab714d3f377a0e871392f1
RayAfolabi <rachaelsadegenesis@gmail.com>
2026-05-27 09:51:10 -0700

    Add print_alphabet function and prototypes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+*Author: Afolabi Rachael
+* Program: WinMingle Community C Training
+* Description:This is a program that prints alphabet in lowercase followed by a new line. it uses _putchar only twice
+*/
+
+void print_alphabet(void){
+    char ch;
+    for(ch = 'a'; ch <= 'z'; ch++) 
+
+ { 
+    
+    _putchar(ch);
+
+  }
+    _putchar('\n');
+ }
+
