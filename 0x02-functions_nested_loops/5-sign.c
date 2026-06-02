d274b3776341d339398540788b4e446b75e765be
RayAfolabi <rachaelsadegenesis@gmail.com>
2026-06-02 16:00:15 -0700

    Add prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*
+* Author: Afolabi Rachael
+* Program: WinMingle Community C Training
+* Description:This is a function that prints the sign of a number
+*/
+int print_sign(int n)
+{
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+       _putchar ('0');
+       return (0);
+    }
+    else
+    {
+        _putchar('-');
+    }   return (-1);
+}
+
+
+
+
