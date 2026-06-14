c15c24a2b78eb05eb079515b8a86c9be6bc67214
RayAfolabi <rachaelsadegenesis@gmail.com>
2026-06-14 15:15:50 -0700

    Add a function that prints 9 timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/*
+* Author: Afolabi Rachael
+* Program: WinMingle Community C Training
+* Description:This is a function that prints 9 times table, starting with 0
+*/
+
+void times_table(void)
+{
+ int row, col, result;
+ 
+ for(row = 0; row <= 9; row++)
+    {
+      for(col = 0; col <= 9; col++)
+     { 
+       result = row * col;
+       if (col ==0)
+       {
+          _putchar(result + '0');
+
+       }
+       else
+       {
+          _putchar(',');    
+          _putchar(' ');
+       if (result < 10)
+      {
+          _putchar(' ');
+          _putchar(result + '0');
+       }
+       else
+       {
+         _putchar((result / 10) +'0');
+         _putchar((result % 10) +'0');
+       }
+      }
+     }
+       _putchar('\n');
+   }
+ }
+
+
+
+
