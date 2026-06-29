99cef3980f68c0ef46e87d05afb3070107f03d25
RayAfolabi <rachaelsadegenesis@gmail.com>
2026-06-29 04:35:50 -0700

    Add a function that adds two integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+* Author: Afolabi Rachael
+* Program: WinMingle Community C Training
+* Description:This is a function that adds two integer
+*/
+
+int add(int a, int b)
+{
+    int sum;
+    sum = a + b;
+    return (sum);
+       
+}
+
+
